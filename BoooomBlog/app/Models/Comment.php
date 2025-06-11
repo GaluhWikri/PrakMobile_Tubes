cf63cda42b078aa87a917869344d10f7faf67bd0
RifqiFadliRachman <rifqifadlir22@gmail.com>
2025-06-11 12:01:38 +0700

    perbaikan fix  model backend

--- BoooomBlog/app/Models/Comment.php
+++ BoooomBlog/app/Models/Comment.php
@@ -3,9 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Import HasFactory
 
 class Comment extends Model
 {
+    use HasFactory; // Add this if you plan to use factories for Comments
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'body',
+        'article_id',
+        'user_id',
+        // 'name' // If you want to store the commenter's name directly and not just rely on user_id->name
+    ];
+
     public function article()
     {
         return $this->belongsTo(Article::class);
